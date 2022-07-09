f036ca2effd29c838b30d440a67914d26f512085
ke-stack <kingkevo254@gmail.com>
2022-07-09 13:33:09 +0300

    commit

--- airline/urls.py
+++ airline/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flights/', include("flights.urls"))
-
+    path('flights/', include("flights.urls")),
+    path('users/', include("users.urls"))
+    
 ]
