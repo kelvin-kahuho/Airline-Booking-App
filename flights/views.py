18b7dd70e9a66f6e52308477831a302706cf6bee
ke-stack <kingkevo254@gmail.com>
2022-07-09 04:21:25 +0300

    commit

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,5 @@
-from audioop import reverse
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-
-import flights
 from .models import Flight, Passenger
 from django.urls import reverse
 
@@ -21,8 +18,8 @@ def flight(request, flight_id):
     })
 
 def book(request, flight_id):
-  if request.method =="POST":
-    flight = Flight.objects.get(pk=flight_id)
-    passenger= Passenger.objects.get(pk=int(request.POST["passenger"]))
-    passenger.flights.add(flight)
-    return HttpResponseRedirect(reverse("flight", args=(flight_id)))
+    if request.method =="POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger= Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
